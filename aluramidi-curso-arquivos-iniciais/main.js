91947b7594ecc9aa18c91d1125abfa5a602dff28
dinailza09 <dinailza09@gmail.com>
2023-02-14 10:43:40 -0300

    adicionando os const

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,40 +1,11 @@
 //Funções dos botões 
 
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom(idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play()
+
 }
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play()
-}
-
-function tocaSomTim() {
-    document.querySelector('#som_tecla_tim').play()
-}
-
-function tocaSomPuff() {
-    document.querySelector('#som_tecla_puff').play()
-}
-
-function tocaSomSplash() {
-    document.querySelector('#som_tecla_splash').play()
-}
-
-function tocaSomToim() {
-    document.querySelector('#som_tecla_toim').play()
-}
-
-function tocaSomPsh() {
-    document.querySelector('#som_tecla_psh').play()
-}
-
-function tocaSomTic() {
-    document.querySelector('#som_tecla_tic').play()
-}
-
-function tocaSomTom() {
-    document.querySelector('#som_tecla_tom').play()
-}
 
 
 //QuerySelectorAll (Seleciona tudo da lista) dos botões
@@ -48,9 +19,19 @@ let contador = 0 ;
 
 //Enquanto 
 while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomPom;
 
+  const tecla = listaDeTeclas[contador];
+
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`;
+
+  //console.log( idAudio)
+
+  tecla.onclick = function(){
+    tocaSom(idAudio);
+  }
     contador = contador + 1;
 
-    console.log(contador)
+   //console.log(contador)
 }
