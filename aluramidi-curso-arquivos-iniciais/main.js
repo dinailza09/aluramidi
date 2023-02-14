67b8e0b9ebe841f7defd0da50e3910f877087b55
dinailza09 <dinailza09@gmail.com>
2023-02-14 09:09:43 -0300

    adicionado as functionr dos botões

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,3 +1,5 @@
+//Funções js
+
 function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play()
 }
@@ -34,20 +36,21 @@ function tocaSomTom() {
     document.querySelector('#som_tecla_tom').play()
 }
 
-document.querySelector('#som_tecla_pom').play();
 
-document.querySelector('#som_tecla_clap').play();
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-document.querySelector('#som_tecla_tim').play();
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
-document.querySelector('#som_tecla_puff').play();
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
 
-document.querySelector('#som_tecla_splash').play();
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
 
-document.querySelector('#som_tecla_toim').play();
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
 
-document.querySelector('#som_tecla_psh').play();
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
 
-document.querySelector('#som_tecla_tic').play();
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
 
-document.querySelector('#som_tecla_tom').play();
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
