20d16ef544dfbc99d79a7cd14dc77e2be8badef3
dinailza09 <dinailza09@gmail.com>
2023-02-14 08:34:18 -0300

    adicionado o querySelector dos botões

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -2,4 +2,20 @@ function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play()
 }
 
+document.querySelector('#som_tecla_pom').play();
+
 document.querySelector('#som_tecla_clap').play();
+
+document.querySelector('#som_tecla_tim').play();
+
+document.querySelector('#som_tecla_puff').play();
+
+document.querySelector('#som_tecla_splash').play();
+
+document.querySelector('#som_tecla_toim').play();
+
+document.querySelector('#som_tecla_psh').play();
+
+document.querySelector('#som_tecla_tic').play();
+
+document.querySelector('#som_tecla_tom').play();
