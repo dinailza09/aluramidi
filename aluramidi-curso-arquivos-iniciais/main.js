bdcffdf7b84d9919144d85e7a7b69e0842c4178a
dinailza09 <dinailza09@gmail.com>
2023-02-15 09:09:28 -0300

    atualizando o projeto

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -4,12 +4,12 @@ function tocaSom(seletorAudio) {
 
    const elemento =  document.querySelector(seletorAudio);
 
-   if (elemento === null) {
-    console.log('Elemento não encontrado!');
-   }
 
    if(elemento!=null && elemento.localName === 'audio'){
        elemento.play();
+   }
+   else{
+     console.log('Elemento não encontrado!');
    }
 }
 
